db183b03eaa9715db6b7a38dc249a0662f540ca6
mahmoud <saqrmadara2000@gmail.com>
2019-06-21 22:29:03 +0200

    final edit

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,6 +2,36 @@ import React, { Component } from "react";
 
 export default class TodoInput extends Component {
   render() {
-    return <div />;
+    const { item, handleChange, handleSubmit, editItem } = this.props;
+    return (
+      <div className='card card-body my-3'>
+        <form onSubmit={handleSubmit}>
+          <div className='input-group'>
+            <div className='input-group-prepend'>
+              <div className='input-group-text bg-primary text-white'>
+                <i className='fas fa-book' />
+              </div>
+            </div>
+            <input
+              type='text'
+              className='form-control text-capitalize'
+              placeholder='add todo item'
+              value={item}
+              onChange={handleChange}
+            />
+            <button
+              type='submit'
+              disabled={item ? false : true}
+              className={
+                editItem
+                  ? "btn btn-block btn-success mt-3"
+                  : "btn btn-primary btn-block mt-3"
+              }>
+              {editItem ? "edit item" : "add item"}
+            </button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 
 export default class TodoItems extends Component {
   render() {
-    return <div />;
+    const { title, handleDelete, handleEdit } = this.props;
+    return (
+      <li className='list-group-item d-flex justify-content-between my-2'>
+        <h6>{title}</h6>
+        <div className='todo-icon'>
+          <span className='mx-2 text-success' onClick={handleEdit}>
+            <i className='far fa-edit' />
+          </span>
+          <span className='mx-2 text-danger' onClick={handleDelete}>
+            <i className='fas fa-trash' />
+          </span>
+        </div>
+      </li>
+    );
   }
 }
